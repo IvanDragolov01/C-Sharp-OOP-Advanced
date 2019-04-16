69f25483a7a56685cc58125b532829008f57f64a
IvanDragolov <sinkara23@abv.bg>
2019-04-16 16:38:55 +0300

    add ex 1 Generic Box of String

---
+++ Generics/01.GenericBox of String/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01.GenericBox_of_String
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int numbersOfStrings = int.Parse(Console.ReadLine());
+			string result = string.Empty;
+
+			for (int i = 0; i < numbersOfStrings; i++)
+			{
+				string text = Console.ReadLine();
+				result += ($"{text.GetType()}: {text.ToString()}");
+				result += Environment.NewLine;
+			}
+
+			Console.WriteLine(result);
+		}
+	}
+}
