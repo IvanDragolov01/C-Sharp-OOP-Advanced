d2faab7bcad11d7ce89eb36cc60c4c61012d5216
IvanDragolov <sinkara23@abv.bg>
2019-04-16 16:49:53 +0300

    add ex 2. Generic Box of Integer

---
+++ Generics/02. Generic Box of Integer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02._Generic_Box_of_Integer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int numbersOfStrings = int.Parse(Console.ReadLine());
+			string result = string.Empty;
+
+			for (int i = 0; i < numbersOfStrings; i++)
+			{
+				int number = int.Parse(Console.ReadLine());
+				result += ($"{number.GetType()}: {number}");
+				result += Environment.NewLine;
+			}
+
+			Console.WriteLine(result);
+		}
+	}
+}
