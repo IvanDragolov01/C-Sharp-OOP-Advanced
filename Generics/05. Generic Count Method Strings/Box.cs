77e0d5ba21e49de8e0083386a85d7ecd61fc1f18
IvanDragolov <sinkara23@abv.bg>
2019-04-17 16:15:08 +0300

    add ex5 40/100

---
+++ Generics/05. Generic Count Method Strings/Box.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace _05.GenericCountMethodStrings
+{
+	public class Box
+	{
+		List<string> allElements = new List<string>();
+		private string _finalElement;
+
+		public void AddInList(string item)
+		{
+			allElements.Add(item);
+		}
+
+		public string GetFinalElement(string element)
+		{
+			_finalElement = element;
+			return _finalElement;
+		}
+
+		public int isEqual()
+		{
+			int equalsNumbers = 0;
+
+			foreach (string element in allElements)
+			{
+				if (element != _finalElement)
+				{
+					equalsNumbers++;
+				}
+			}
+
+			return equalsNumbers;
+		}
+	}
+}
--- Generics/05. Generic Count Method Strings/Program.cs
+++ Generics/05. Generic Count Method Strings/Program.cs
@@ -1,19 +1,23 @@
 ﻿using System;
 
-namespace _5._Generic_Count_Method_Strings
+namespace _05.GenericCountMethodStrings
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
 			int n = int.Parse(Console.ReadLine());
-			Box box = new Box;
+			Box box = new Box();
 
 			for (int i = 0; i < n; i++)
 			{
 				string text = Console.ReadLine();
 				box.AddInList(text);
 			}
+
+			string finalString = Console.ReadLine();
+			box.GetFinalElement(finalString);
+			Console.WriteLine(box.isEqual());
 		}
 	}
 }
