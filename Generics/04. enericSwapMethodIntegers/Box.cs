489e39d83f5cd6399d839154e30352076387c893
IvanDragolov <sinkara23@abv.bg>
2019-04-17 15:44:32 +0300

    add box cs

---
+++ Generics/04. enericSwapMethodIntegers/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04._enericSwapMethodIntegers
+{
+	public class Box<T>
+	{
+		private T _item;
+
+		public Box(T item)
+		{
+			_item = item;
+		}
+
+		public override string ToString()
+		{
+			return $"{_item.GetType().FullName}: {_item}";
+		}
+	}
+}
