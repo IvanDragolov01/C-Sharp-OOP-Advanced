80b75cd5f75d62224f30eccdcf520149383b7ca6
IvanDragolov <sinkara23@abv.bg>
2019-04-17 14:41:46 +0300

    add ex 3 GenericSwapMethodString

--- Generics/03.GenericSwapMethodString/Program.cs
+++ Generics/03.GenericSwapMethodString/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _03.GenericSwapMethodString
 {
@@ -6,7 +7,29 @@ namespace _03.GenericSwapMethodString
 	{
 		static void Main(string[] args)
 		{
-			
+			int n = int.Parse(Console.ReadLine());
+			List<string> text = new List<string>();
+
+			for (int i = 0; i < n; i++)
+			{
+				string line = Console.ReadLine();
+				text.Add(line);
+			}
+
+			//int[] masiv = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+			string[] numbers = Console.ReadLine()
+				.Split(" ");
+
+			int swapNumbers = int.Parse(numbers[0]);
+			int swapNumbers2 = int.Parse(numbers[1]);
+			string forChange = text[swapNumbers];
+			text[swapNumbers] = text[swapNumbers2];
+			text[swapNumbers2] = forChange;
+
+			foreach (string words in text)
+			{
+				Console.WriteLine($"{words.GetType()}: {words}");
+			}
 		}
 	}
 }
