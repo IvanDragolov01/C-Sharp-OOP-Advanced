c29975b4747eae73909c45b34d6a3ab209e2d93b
IvanDragolov <sinkara23@abv.bg>
2019-04-17 15:17:28 +0300

    add template

---
+++ Generics/5. Generic Count Method Strings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _5._Generic_Count_Method_Strings
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+			Box box = new Box;
+
+			for (int i = 0; i < n; i++)
+			{
+				string text = Console.ReadLine();
+				box.AddInList(text);
+			}
+		}
+	}
+}
