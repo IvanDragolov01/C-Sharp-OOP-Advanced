ad9e7e717a1a3e05b3e95ea3cdd4e4bfc4798656
IvanDragolov <sinkara23@abv.bg>
2019-04-18 14:32:36 +0300

    add template only

---
+++ Generics/10.Tuple/Tuple.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _10.Tuple
+{
+	class Tuple
+	{
+		private  object item1;
+		private  object item2;
+
+		private var population = new Tuple(object item1,object item2);
+	}
+}
