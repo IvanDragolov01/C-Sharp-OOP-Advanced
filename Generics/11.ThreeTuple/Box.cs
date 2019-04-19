4a5840a6356b61bfe756d23c2646cf3755254eff
IvanDragolov <sinkara23@abv.bg>
2019-04-19 14:15:40 +0300

    add ex 11 ThreeTuple

---
+++ Generics/11.ThreeTuple/Box.cs
@@ -0,0 +1,21 @@
+﻿namespace _11.ThreeTuple
+{
+	public class Tuple<T, P, G>
+	{
+		private T _firstItem;
+		private P _secondItem;
+		private G _thirdItem;
+
+		public Tuple(T firstItem, P secondItem, G thirdItem)
+		{
+			_firstItem = firstItem;
+			_secondItem = secondItem;
+			_thirdItem = thirdItem;
+		}
+
+		public override string ToString()
+		{
+			return $"{_firstItem} -> {_secondItem} -> {_thirdItem}";
+		}
+	}
+}
---
+++ Generics/11.ThreeTuple/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _11.ThreeTuple
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] input = Console.ReadLine().Split(" ");
+			string name = string.Concat(input[0], input[1]);
+			string address = input[2];
+			string town = input[3];
+			Tuple<string, string, string> nameAndAddressAndTown = new Tuple<string, string, string>(name, address, town);
+			Console.WriteLine(nameAndAddressAndTown.ToString());
+
+			input = Console.ReadLine().Split(" ");
+			name = input[0];
+			int litersOfBeer = int.Parse(input[1]);
+			string word = input[2];
+			bool IsDrunk = false;
+
+			if (word == "drunk")
+			{
+				IsDrunk = true;
+			}
+			else
+			{
+				IsDrunk = false;
+			}
+
+			Tuple<string, int, bool> nameAndBeerLiters = new Tuple<string, int, bool>(name, litersOfBeer, IsDrunk);
+			Console.WriteLine(nameAndBeerLiters.ToString());
+
+			input = Console.ReadLine().Split(" ");
+			string nameOfUser = input[0];
+			double accountBalance = double.Parse(input[1]);
+			string bankName = input[2];
+			Tuple<string, double, string> integerAndDouble = new Tuple<string, double, string>(nameOfUser, accountBalance, bankName);
+			Console.WriteLine(integerAndDouble.ToString());
+		}
+	}
+}
