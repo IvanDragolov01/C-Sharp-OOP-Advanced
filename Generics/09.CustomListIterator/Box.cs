98417d9f9f145c3ab4876e953cf28004c16bf6d4
IvanDragolov <sinkara23@abv.bg>
2019-04-19 13:28:50 +0300

    not working template

--- Generics/09.CustomListIterator/Box.cs
+++ Generics/09.CustomListIterator/Box.cs
@@ -1,10 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace _09.CustomListIterator
 {
-	class Box
+	public class Box 
 	{
+		private List<string> elements = new List<string>();
+
+		public void Add(string element)
+		{
+			elements.Add(element);
+		}
+
+		public void Remove(int index)
+		{
+			elements.Remove(elements[index]);
+		}
+
+		public void IsContains(string word)
+		{
+			if (elements.Contains(word))
+			{
+				Console.WriteLine("True");
+			}
+			else
+			{
+				Console.WriteLine("False");
+			}
+		}
+
+		public void PrintAllElements()
+		{
+			foreach (string element in elements)
+			{
+				Console.WriteLine(element);
+			}
+		}
+
+		public void SwapElements(int x, int y)
+		{
+			string element = elements[x];
+			elements[x] = elements[y];
+			elements[y] = element;
+		}
+
+		public void Greater(string greaterWord)
+		{
+			int counter = 0;
+
+			foreach (string element in elements)
+			{
+				if (element.CompareTo(greaterWord) > 0)
+				{
+					counter++;
+				}
+			}
+
+			Console.WriteLine(counter);
+		}
+
+		public void PrintMaxElement()
+		{
+			Console.WriteLine(elements.Max());
+		}
+
+		public void SortElements()
+		{
+			elements.Sort();
+		}
+
+		public void PrintMinElement()
+		{
+			Console.WriteLine(elements.Min());
+		}
 	}
 }
--- Generics/09.CustomListIterator/Program.cs
+++ Generics/09.CustomListIterator/Program.cs
@@ -6,7 +6,50 @@ namespace _09.CustomListIterator
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string[] command = Console.ReadLine().Split(" ");
+			Box box = new Box();
+
+			while (command[0] != "END")
+			{
+				switch (command[0])
+				{
+					case "Add":
+						string word = command[1];
+						box.Add(word);
+						break;
+					case "Remove":
+						int indexToRemove = int.Parse(command[1]);
+						box.Remove(indexToRemove);
+						break;
+					case "Contains":
+						string wordIsContains = command[1];
+						box.IsContains(wordIsContains);
+						break;
+					case "Swap":
+						int indexOne = int.Parse(command[1]);
+						int indexTwo = int.Parse(command[2]);
+						box.SwapElements(indexOne, indexTwo);
+						break;
+					case "Greater":
+						string greaterWord = command[1];
+						box.Greater(greaterWord);
+						break;
+					case "Max":
+						box.PrintMaxElement();
+						break;
+					case "Min":
+						box.PrintMinElement();
+						break;
+					case "Print":
+						box.PrintAllElements();
+						break;
+					case "Sort":
+						box.SortElements();
+						break;
+				}
+
+				command = Console.ReadLine().Split();
+			}
 		}
 	}
 }
